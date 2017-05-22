56f1771bf08e97e06e2eb82cce0d61c487dd538e
Klemen Košir <klemen.kosir@kream.io>
2017-05-22 20:41:28 +0200

    DO NOT USE OR YOU WILL BE FIRED

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import ev3dev.ev3 as ev3
-import time
 import subprocess
+import sys
+import time
 
 
 def FastRead(infile):
@@ -34,11 +35,14 @@ def init_gyro():
     g.mode = "GYRO-RATE"
     g.mode = "GYRO-ANG"
 
-    time.sleep(GYRO_NUM_CALIBRATION_LOOPS * LOOP_TIME)
-    g_error = g.value()
-    g_error_rate = g_error / GYRO_NUM_CALIBRATION_LOOPS
+    g_error_rate = 0.0
 
-    return g, g_error, g_error_rate
+    for _ in range(GYRO_NUM_CALIBRATION_LOOPS):
+        g_error_rate += g.value()
+        time.sleep(LOOP_TIME)
+    g_error_rate /= GYRO_NUM_CALIBRATION_LOOPS
+
+    return g, 0.0, g_error_rate
 
 
 def init_sensors():
@@ -102,7 +106,9 @@ def move():
         left_speed = base_speed
         right_speed = base_speed
 
+        # angle = abs(GYRO.value() % 360) - GYRO_ERROR
         angle = abs(GYRO.value() % 360) - GYRO_ERROR
+        F.write(str("%.2f\n" % angle))
         angle_error = angle_goal - angle
 
         if abs(angle_error) > 180:
@@ -148,11 +154,21 @@ def move():
                 integral = 0.0
                 previous_angle_error = 0.0
 
+                if angle_goal == 180:
+                    rotate(150)
+                elif angle_goal == 0:
+                    rotate(30)
+
                 rotate(angle_goal)
+
+                if angle_goal == 180:
+                    GYRO_ERROR = 2 * (current_line / 5)
+                elif angle_goal == 0:
+                    GYRO_ERROR = 3 * (current_line / 5)
+
                 continue
 
         time_diff = time.time() - time_start
-        F.write(str("%.2f\n" % time_diff))
         if time_diff < LOOP_TIME:
             time.sleep(LOOP_TIME - time_diff)
 
@@ -165,11 +181,27 @@ def move():
     stop_motors()
 
 
+LAST_DIRECTION = -1
+
 def rotate(angle_goal):
     global GYRO_ERROR
+    global LAST_DIRECTION
 
     integral = 0.0
     previous_angle_error = 0.0
+
+    success = 0
+    direction = 0
+
+    # angle = abs(GYRO.value() % 360) - GYRO_ERROR
+    # angle = abs(GYRO.value() % 360) - GYRO_ERROR
+    # angle_error = angle_goal - angle
+
+    # if abs(angle_error) > 180:
+    #     angle_error = (angle_error + 360) if (angle_error < 0) else (angle_error - 360)
+    #
+    # if angle_error < 0:
+    #     direction = 1
 
     while True:
         time_start = time.time()
@@ -189,11 +221,11 @@ def rotate(angle_goal):
         previous_angle_error = angle_error
         speed_correction = P_ROTATE * proportion + I_ROTATE * integral + D_ROTATE * derivative
 
-        asd = 30.0
+        asd = 35.0
         if abs(speed_correction) < asd:
             speed_correction = asd if (speed_correction > 0) else -asd
 
-        qwe = 45.0
+        qwe = 50.0
         if abs(speed_correction) > qwe:
             speed_correction = qwe if (speed_correction > 0) else -qwe
 
@@ -201,7 +233,17 @@ def rotate(angle_goal):
                 (angle, angle_error, integral, derivative, speed_correction))
 
         # TODO: Move up.
-        if abs(angle_error) < 1:
+        if abs(angle_error) < 2:
+            break
+            success += 1
+        else:
+            success = 0
+
+        if success == 5:
+            # F.write("%.2f\n" % GYRO.value())
+            # stop_motors()
+            # F.write("%.2f\n" % GYRO.value())
+            # sys.exit()
             break
 
         left_speed = speed_correction
@@ -210,7 +252,6 @@ def rotate(angle_goal):
         set_motors(left_speed, right_speed)
 
         time_diff = time.time() - time_start
-        F.write(str("%.2f\n" % time_diff))
         if time_diff < LOOP_TIME:
             time.sleep(LOOP_TIME - time_diff)
 
@@ -218,47 +259,81 @@ def rotate(angle_goal):
 
         GYRO_ERROR += GYRO_ERROR_RATE
 
+    if LAST_DIRECTION == -1:
+        if direction == 1:
+            GYRO_ERROR += GYRO_MANUAL_CORRECTION
+        else:
+            GYRO_ERROR -= GYRO_MANUAL_CORRECTION
+    elif LAST_DIRECTION == direction:
+        if direction == 1:
+            GYRO_ERROR += GYRO_MANUAL_CORRECTION
+        else:
+            GYRO_ERROR -= GYRO_MANUAL_CORRECTION
+
+    LAST_DIRECTION = direction
+
     stop_motors()
 
 
 LOOP_TIME = 0.01
-GYRO_NUM_CALIBRATION_LOOPS = 500
+GYRO_NUM_CALIBRATION_LOOPS = 100
 
-START_SPEED = 40.0
+START_SPEED = 20.0
 SPEED = 90.0
 MAX_SPEED = 100.0
 
-BREAK_TIME = 0.5
+BREAK_TIME = 1.0
 
-ACCELERATION_STEP_TIME = 1.0
+ACCELERATION_STEP_TIME = 0.25
 ACCELERATION_STEP_SPEED = 5.0
 ACCELERATION_STEP_SPEED_FACTOR = 1.0
 
 LINE_COLOR = 10
-LINE_TIME = 0.5
+LINE_TIME = 0.25
 FLOOR_COLOR = 20
 
 ROTATION_LINES = [2, 4, 7, 10, 14]
+# ROTATION_LINES = [4, 8]
 END_LINE = 18
 
-P_MOVE = 1.2
-I_MOVE = 0.1
-I_MOVE_FACTOR = 0.75
-D_MOVE = 0.5
+# P_MOVE = 1.2
+# I_MOVE = 0.1
+# I_MOVE_FACTOR = 0.75
+# D_MOVE = 0.5
 
-# P_MOVE = 1.65
-# I_MOVE = 0.066
-# I_MOVE_FACTOR = 1.0
-# D_MOVE = 10.3125
+P_MOVE = 1.65
+I_MOVE = 0.066
+I_MOVE_FACTOR = 1.0
+D_MOVE = 10.3125
 
-P_ROTATE = 1.0
+# P_MOVE = 3.0
+# I_MOVE = 0.2
+# I_MOVE_FACTOR = 0.8
+# D_MOVE = 5.0
+
+GYRO_MANUAL_CORRECTION = 0
+
+# Kc = 5.0
+# wobble = 0.25
+#
+# P_MOVE = 0.6 * Kc
+# I_MOVE = 2 * P_MOVE * LOOP_TIME / wobble
+# I_MOVE_FACTOR = 0.75
+# D_MOVE = P_MOVE * wobble / (8 * LOOP_TIME)
+
+print(P_MOVE)
+print(I_MOVE)
+print(D_MOVE)
+
+P_ROTATE = 0.5
 I_ROTATE = 0.0
-I_ROTATE_FACTOR = 0.0
-D_ROTATE = 0.0
+I_ROTATE_FACTOR = 1.0
+D_ROTATE = 1.0
 
 LEFT_MOTOR, RIGHT_MOTOR = init_motors()
 GYRO, GYRO_ERROR, GYRO_ERROR_RATE = init_gyro()
 print(GYRO_ERROR_RATE)
+# GYRO_ERROR_RATE = 0.003
 COLOR = init_sensors()
 RESET = init_reset()
 
@@ -290,5 +365,24 @@ with open('ev3devices/outD/command', 'w') as f:
     f.write('run-direct')
 
 move()
+
+# rotate(90)
+# rotate(180)
+# stop_motors()
+# rotate(90)
+# rotate(0)
+# stop_motors()
+# rotate(90)
+# rotate(180)
+# stop_motors()
+# rotate(90)
+# rotate(0)
+# stop_motors()
+# rotate(90)
+# rotate(180)
+# stop_motors()
+# rotate(90)
+# rotate(0)
+# stop_motors()
 
 F.close()
